49371c0397c2a678dc2539def286d51344fcc428
romdotdog <romdotdog@users.noreply.github.com>
2021-04-14 18:42:05 -0500

    Pass initial counter value to `new Chacha20`

--- chacha20.js
+++ chacha20.js
@@ -109,7 +109,7 @@ exports.Cipher = Chacha20;
 
 exports.encrypt = exports.decrypt = function(key, nonce, data)
 {
-  var cipher = new Chacha20(key, nonce);
+  var cipher = new Chacha20(key, nonce, 1);
   var ret = new Buffer(data.length);
   cipher.encrypt(ret, data, data.length);
   return ret;
